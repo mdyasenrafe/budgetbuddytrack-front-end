e4ef05d78c3905efd51541d106a73543d1483023
Md yasen rafe <mdyasenrafe@gmail.com>
2024-02-21 15:55:57 +0600

    font setup done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { Typography } from "./src/theme/typography";
 
 export default function App() {
+  const [loaded] = useFonts({
+    PoppinsRegular: require("./assets/fonts/Poppins-Regular.ttf"),
+    UrbanistMedium: require("./assets/fonts/Urbanist-Medium.ttf"),
+    UrbanistBold: require("./assets/fonts/Urbanist-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text
+        style={{
+          fontFamily: Typography.bold,
+        }}
+      >
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
